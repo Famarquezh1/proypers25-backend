247dfdf8d099f2695627ca27cba710c024841ffa
Famarquezh1 <93333681+Famarquezh1@users.noreply.github.com>
2026-09-09 09:22:52 -0300

    test(spot): allow XEC to re-enter opportunity engine

--- backend/tests/protectedReserveAsset.test.js
+++ backend/tests/protectedReserveAsset.test.js
@@ -6,11 +6,13 @@ const {
   isProtectedReserveSymbol
 } = require('../services/approvedSpotRealExecutor');
 
-assert.deepStrictEqual(HARD_PROTECTED_RESERVE_ASSETS, ['XEC']);
-assert.strictEqual(isProtectedReserveSymbol('XECUSDT'), true);
-assert.strictEqual(isProtectedReserveSymbol('xecusdt'), true);
+assert.deepStrictEqual(HARD_PROTECTED_RESERVE_ASSETS, []);
+assert.strictEqual(isProtectedReserveSymbol('XECUSDT'), false);
+assert.strictEqual(isProtectedReserveSymbol('xecusdt'), false);
 assert.strictEqual(isProtectedReserveSymbol('QTUMUSDT'), false);
 assert.strictEqual(isProtectedReserveSymbol('QTUMUSDT', { protected_assets: ['QTUM'] }), true);
+// Legacy protected_assets config must not freeze XEC out of the normal engine.
+assert.strictEqual(isProtectedReserveSymbol('XECUSDT', { protected_assets: ['XEC'] }), false);
 assert.strictEqual(isProtectedReserveSymbol('XECBTC'), false);
 
 console.log('protected reserve asset tests passed');
